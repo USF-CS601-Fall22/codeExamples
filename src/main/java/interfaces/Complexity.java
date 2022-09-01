83b4a8541966e4149752aca5fd10d2228be5a289
Olga Karpenko <okarpenko@usfca.edu>
2022-09-01 10:08:21 -0700

    Updated interface examples.

---
+++ src/main/java/interfaces/Complexity.java
@@ -0,0 +1,12 @@
+package interfaces;//********************************************************************
+//  Complexity.java       Author: Lewis/Loftus
+//
+//  Represents the interface for an object that can be assigned an
+//  explicit complexity.
+//********************************************************************
+
+public interface Complexity
+{
+   void setComplexity(int complexity);
+   int getComplexity();
+}
